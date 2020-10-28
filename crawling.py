eed0fedc2d3a263d7ec2bebbe20d80533ff11e4a
ceed56 <ceed56@paran.com>
2020-10-28 15:06:09 +0900

    e

--- crawling.py
+++ crawling.py
@@ -26,7 +26,7 @@ driverlist  = []
 
 global options
 options = webdriver.ChromeOptions()
-# options.add_argument('--headless')
+options.add_argument('--headless')
 options.add_argument("--incognito")
 options.add_argument("--disable-notifications")
 options.add_argument('--no-sandbox')
